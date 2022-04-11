741e94ba62d98bc6772591a2f834404618e7157c
Brad Brous <brbr51@tampabay.rr.com>
2022-04-11 12:14:58 -0400

    more donut formatting

--- src/pages/public/sampleSite/samChits/twoParty/DonutGraph.jsx
+++ src/pages/public/sampleSite/samChits/twoParty/DonutGraph.jsx
@@ -2,15 +2,40 @@ import React from 'react';
  
 import { PieChart, ResponsiveContainer, Pie,  Sector, Cell, Tooltip, Legend } from 'recharts'
 
+
+const data = [{name: 'Group A', value: 300}, {name: 'Group B', value: 600}];
+
 export default function DonutGraph(props) {
   const COLORS = [  'red', '#00C49F'];
 
 const RADIAN = Math.PI / 180;     
-  return (
-    <>
-      ConutGraph here
-    </>
-  );
+return (
+  <ResponsiveContainer width="100%" height="100%" >
+  <PieChart  >
+
+    <Pie
+      data={data} 
+      cx="50%" 
+      cy="50% "
+      startAngl = '90'
+      endAngle = '180'
+      innerRadius="80%"
+      outerRadius="100%" 
+      fill="#8884d8"
+      paddingAngle={4}
+  
+    >
+    
+      {
+        data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+      }
+     
+    </Pie>
+   
+  </PieChart>
+  </ResponsiveContainer>
+);
 }
+
 
  
--- src/pages/public/sampleSite/samChits/twoParty/KarmicView_s.jsx
+++ src/pages/public/sampleSite/samChits/twoParty/KarmicView_s.jsx
@@ -26,133 +26,223 @@ import { styled, createTheme} from "@mui/material/styles"
 const theme = createTheme(); // allows use of mui theme in styled c
  
  
-const Wrapper= styled(Paper)({
+const Wrapper = styled(Paper)({
 
-  // backgroundColor: 'green' ,
-  
-    display: 'flex',
-    position: 'relative',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    width: 'calc(100% - 16px)' ,
-    height: '30rem',
-  
-  
+ 
+
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  width: 'calc(100% - 16px)',
+  height: '30rem',
+
+
   //   [theme.breakpoints.down('sm')] : {
   //     // width: '100%'
   //   },
-  
-  })
 
-  const TopWrapper= styled('div')({
+})
 
-    
-    
-      display: 'flex',
-      position: 'relative',
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-      width: '100%' ,
+const TopWrapper = styled('div')({
+
+
+
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  width: '100%',
+
  
-      // backgroundColor: 'yellow'
-    
-    
-    //   [theme.breakpoints.down('sm')] : {
-    //     // width: '100%'
-    //   },
-    
-    })
 
-    const BottomWrapper= styled('div')({
 
-     
-      
-        display: 'flex',
-        position: 'relative',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        width: '100%' ,
-        height: '75%',
-   
-        backgroundColor: 'green' ,
-      
-      
-      //   [theme.breakpoints.down('sm')] : {
-      //     // width: '100%'
-      //   },
-      
-      })
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
 
-      const LeftBottomWrapper= styled('div')({
+})
 
-     
-      
-        display: 'flex',
-        position: 'relative',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '40%' ,
-        height: '100%',
-   
-        backgroundColor: 'yellow' ,
-      
-      
-      //   [theme.breakpoints.down('sm')] : {
-      //     // width: '100%'
-      //   },
-      
-      })
-    
-      const RightBottomWrapper= styled('div')({
+const BottomWrapper = styled('div')({
 
-     
-      
-        display: 'flex',
-        position: 'relative',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '40%' ,
-        height: '100%',
-   
-        backgroundColor: 'pink' ,
-      
-      
-      //   [theme.breakpoints.down('sm')] : {
-      //     // width: '100%'
-      //   },
-      
-      })
+
+
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '100%',
+  height: '75%',
+
+
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
+
+const LeftBottomWrapper = styled('div')({
+
+
+
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  width: '40%',
+  height: '100%',
+ 
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
+
+const RightBottomWrapper = styled('div')({
+
+
+
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '40%',
+  height: '100%',
+
+ 
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
+
+
+
+
+const GraphWrapper = styled('div')({
+
+ 
+
+  display: 'flex',
+
+
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  width: '100%',
+  height: '75%',
+  marginTop: '5vh'
+
+
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
+
+const CenterValue = styled('div')({
+
+  position: 'absolute',
+  // left: '50%',
+  top: '40%',
+  display: 'block',
+  color: '#2B6D6D',
+
+
+  fontSize: '2rem',
+
+  textAlign: 'justify',
+  '& .emphasize': {
+    decoration: 'underline',
+    fontWeight: 'bold'
+  },
+
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
+
+
+const NoteWrapper = styled('div')({
+
+
+
+  display: 'block',
+  color: '#2B6D6D',
+  height: '3rem',
+  width: '50%',
+  fontSize: '.75rem',
+
+
+
+
+  //   [theme.breakpoints.down('sm')] : {
+  //     // width: '100%'
+  //   },
+
+})
 
       // ========================================
 export default function KarmicView_s(props) {
   return (
     <Wrapper>
       <TopWrapper>
-        
-      <KarmicLegend/>
+
+        <KarmicLegend />
       </TopWrapper>
 
-   <BottomWrapper>
-     
-     <LeftBottomWrapper>
-     <div>Karmic Balance</div>
-     <DonutGraph/> 
-     <div>Description</div>
-     </LeftBottomWrapper>
+      <BottomWrapper>
+        <LeftBottomWrapper>
+          <div> Karmic Balance</div>
+          <GraphWrapper>
 
-     <RightBottomWrapper>
-     <div># of Chits</div>
-     <DonutGraph/> 
-     <div>Description</div>
-     </RightBottomWrapper>
-   
-   </BottomWrapper>
-     
+            < DonutGraph />
+            <CenterValue>45</CenterValue>
+
+          </GraphWrapper>
+          <NoteWrapper>
+            Your <span className='emphasize'>
+              karmic balance </span> is the
+            sum total of weights you assigned to all of your
+            two party chits.
+            <div className='small'> (chits you owe - chits owed to you) </div>
+          </NoteWrapper>
+
+
+        </LeftBottomWrapper>
+
+
+        <RightBottomWrapper>
+          <div> # of Chits</div>
+          <GraphWrapper>
+
+            < DonutGraph />
+            <CenterValue>45</CenterValue>
+
+          </GraphWrapper>
+          <NoteWrapper>
+
+          </NoteWrapper>
+        </RightBottomWrapper>
+
+      </BottomWrapper>
+
 
 
 
