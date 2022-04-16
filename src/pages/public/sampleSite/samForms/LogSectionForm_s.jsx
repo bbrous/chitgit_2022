766ae2fb1535529edf6be2452b5a3382b4f431ff
Brad Brous <brbr51@tampabay.rr.com>
2022-04-16 09:45:38 -0400

    log form style setup

--- src/pages/public/sampleSite/samForms/LogSectionForm_s.jsx
+++ src/pages/public/sampleSite/samForms/LogSectionForm_s.jsx
@@ -55,7 +55,7 @@ import Button from '@mui/material/Button'
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 import { styled, createTheme} from '@mui/material/styles'
-import { veryLightGrey } from '../../../../styles/colors'
+import { chitBlueDull, mediumGrey, veryLightGrey } from '../../../../styles/colors'
 
 const theme = createTheme(); // allows use of mui theme in styled component
 
@@ -79,26 +79,7 @@ backgroundColor: veryLightGrey,
     },
   
   })
-
-  const HeaderWrapper = styled('div')({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: '.5rem 0 .5rem 0',
-    marginBottom: '.5rem',
-    borderBottom: '2px solid #CFD0D1',
-    boxShadow : '0 0 1px 0 #F6F7F8' ,
-    // zIndex: '95',
-
-    width: '100%',
-
-    [theme.breakpoints.down('sm')]: {
-      // height: '1.25rem',
-      // backgroundColor: 'red'
-    },
-  
-  })
+ 
 
   const FormWrapper = styled('form')({
     position: 'relative',
@@ -119,54 +100,267 @@ backgroundColor: veryLightGrey,
     },
   
   })
-  const FormComponentWrapper= styled('div')({
-    position: 'relative',
+
+  const MainWrapper= styled('div')({
+
     display: 'flex',
+    position: 'relative',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: 'green',
     width: '100%',
-    margin: '.5rem',
-  
-   
-    [theme.breakpoints.down('sm')]: {
-      // height: '1.25rem',
-  
+    height: '100%',
+    marginBottom: '6px',
+    paddingBottom: '6px',
+  overflow: 'auto',
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
     },
+  
   
   })
   
-  const ComponentName= styled('div')({
+ 
+  const TopWrapper= styled('div')({
+
     display: 'flex',
+    position: 'relative',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: veryLightGrey,
     width: '100%',
-    color: 'darkGrey',
-  
-  
-    [theme.breakpoints.down('sm')]: {
-      // height: '1.25rem',
-  
+    margin: '3px 0',
+    borderRadius: '5px 5px 0 0',
+    borderLeft: '1px solid #CFD0D1',
+    borderTop: '1px solid #CFD0D1',
+    borderRight: '1px solid #CFD0D1',
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
     },
   
-  })
   
-  const ComponentWrapper= styled('div')({
+  })
+
+  const DateWrapper= styled('div')({
+
     display: 'flex',
+    position: 'relative',
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    // backgroundColor: 'green',
+    width: '50%',
+    padding: '2px 6px',
+  
+    fontSize: '.8rem',
+    height: '.8rem',
+  
+    '& span': {
+      color: mediumGrey,
+      marginLeft: '6px',
+       
+    },
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+
+  const OuterContentWrapper= styled(Paper)({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'Column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: 'aqua',
     width: '100%',
+    borderLeft: '1px solid #CFD0D1',
+    borderRight: '1px solid #CFD0D1',
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+  const ContentWrapper= styled('div')({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'row',
+  
+    backgroundColor: veryLightGrey,
+    width: '100%',
+    // marginTop:'6px',
+    margin: 'auto',
+    
+    
+    
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+
+  const MetaWrapper= styled('div')({
+ 
+    display: 'flex',
+   
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    fontSize: '.75rem',
+    width: '30%',
+    minHeight: '100%',
+   
+    padding: '6px',
+    backgroundColor: veryLightGrey,
   
    
-    [theme.breakpoints.down('sm')]: {
-      // height: '1.25rem',
   
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
     },
+  
+  
+  })
+
+  const TimesWrapper= styled('div')({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  
+    width: '100%',
+    padding: '2px 6px',
+  
+    fontSize: '.65rem',
+    color: mediumGrey,
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
   
   })
   
+  const HeadlineWrapper= styled('div')({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    color: chitBlueDull,
+    width: '99%',
+    padding: '6px 0',
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+  const SearchWrapper= styled('div')({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: veryLightGrey,
+    width: '99%',
+    padding: '2px 0',
+    margin: '3px 0',
+  // backgroundColor: veryLightGrey,
+    fontSize: '.6rem',
+    height: '.8rem',
+    color: mediumGrey,
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+
+
+  const PeopleWrapper= styled('div')({
+
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: 'aqua',
+    width: '30%',
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+  
+  const KeyWordWrapper= styled('div')({
+  
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: 'orange',
+    width: '70%',
+  
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+
+
+  const Content= styled('div')({
+    flexGrow: 1,
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    fontSize: '.85rem',
+    width: '70%',
+  
+    padding: '6px',
+    borderLeft: '1px solid #E6E7E8',
+    backgroundColor: 'white',
+    borderRadius: '0 5px 5px 0',
+    [theme.breakpoints.down('sm')] : {
+      // width: '100%'
+    },
+  
+  
+  })
+
+
+
+
+
+
+
+//  --- Buttons -----------
   const ButtonWrapper= styled('div')({
     display: 'flex',
     flexDirection: 'row',
@@ -190,24 +384,7 @@ backgroundColor: veryLightGrey,
   })
 
 
-  const StyledCalendarIcon = styled(CalendarTodayIcon)({
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: '8px',
-    width: '16px',
-    color: '#CFD0D1',
-  
-   
-  
-    [theme.breakpoints.down('sm')]: {
-      // height: '1.25rem',
-  
-    },
-  
-  })
+ 
 
 
 
@@ -273,7 +450,42 @@ export default function LogForm_s(props) {
 <FormProvider>
 <FormWrapper id="submit-form" onSubmit={handleSubmit(submitForm) } >
 
-<div>  Log Form Here </div>
+<MainWrapper>
+      <TopWrapper>
+        <DateWrapper>date</DateWrapper>
+
+      </TopWrapper>
+  
+    <OuterContentWrapper> 
+
+      <ContentWrapper>
+
+        <MetaWrapper>
+       meta
+          <TimesWrapper>
+              timestamp:   
+            
+          </TimesWrapper>
+        </MetaWrapper>
+        
+     
+        <Content>
+          <HeadlineWrapper> title</HeadlineWrapper>
+    
+          content
+        </Content>
+
+
+      </ContentWrapper>
+      <SearchWrapper>
+        <PeopleWrapper>people</PeopleWrapper>
+        <KeyWordWrapper>  keywords</KeyWordWrapper>
+
+      </SearchWrapper>
+      </OuterContentWrapper>
+
+
+    </MainWrapper>
   
         {/* ------Submit ---------- -------------------------- */}
         <ButtonWrapper>
